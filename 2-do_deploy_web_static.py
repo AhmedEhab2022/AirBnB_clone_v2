7230062b1499946428b323a3aa964679cecab2e5
root <am8122004@gmail.com>
2023-12-06 11:42:33 -0800

    2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module contain fabric script with do_deploy function
+"""
+from fabric.api import env, put, run
+import os
+
+
+env.hosts = ['54.157.128.50', '100.25.20.8']
+
+
+def do_deploy(archive_path):
+    """
+    Fabric script (based on the file 1-pack_web_static.py)
+    that distributes an archive to your web servers,
+    using the function do_deploy
+    """
+
+    if not os.path.exists(archive_path):
+        return False
+
+    put(archive_path, '/tmp/')
+    archive_name = archive_path.split("/")[-1]
+    no_ext = archive_name.split(".")[0]
+    unCompPath = "/data/web_static/releases"
+    run("mkdir -P {}{}/".format(unCompPath, no_ext))
+    run("tar -xzf /tmp/{} -C {}/{}/".format(archive_name, unCompPath, no_ext))
+    run("rm /tmp/{}".format(archive_name))
+    run("mv {}/{}/web_static/* {}/{}/".format(unCompPath, no_ext,
+                                              unCompPath, no_ext))
+
+    run("rm -rf {}/{}/web_static".format(unCompPath, no_ext))
+    run("rm -rf /data/web_static/current")
+    run("ln -s {}/{}/ /data/web_static/current".format(unCompPath, no_ext))
+    return True
