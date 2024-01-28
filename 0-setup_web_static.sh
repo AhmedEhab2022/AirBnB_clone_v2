3ab52e8b98e122b4262ad9a7075808a5f1db6818
AhmedEhab2022 <am8122004@gmail.com>
2024-01-28 11:09:58 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,35 +4,57 @@
 sudo apt-get update -y
 sudo apt-get install nginx -y
 
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+
+sudo echo "<html>
   <head>
   </head>
   <body>
-    Holberton School
+    Test Nginx configuration
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-echo "
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+
+sudo bash -c 'cat > /etc/nginx/sites-available/default <<EOF
 server {
-    listen 80;
-    listen [::]:80;
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-    server_name ahmedehabdom.tech;
+    add_header X_Served_By \$hostname;
 
-    location /hbnb_static/ {
-    	alias /data/web_static/current;
-        index index.html;
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
 
     location /redirect_me {
-     	return 301 https://ahmedehabdom.tech/hbnb_static/;
+        return 301 https://google.com/;
     }
-}" > /etc/nginx/sites-available/default
+
+    error_page 404 /404.html;
+
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+EOF'
+
 sudo systemctl restart nginx
